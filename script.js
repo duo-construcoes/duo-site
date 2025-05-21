0292ff2309bc753565b5bb875f84b496723682f0
Rodrigo Evangelista <contato@duoconstrucoes.com.br>
2025-05-21 19:39:11 -0300

    Corrige leitura do campo Mini Currículo com codificação real

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function carregarDadosCSV(url, callback) {
   });
 }
 
-// SEÇÃO EQUIPE
+// EQUIPE - com campo "Mini CurrÃ­culo"
 carregarDadosCSV('https://docs.google.com/spreadsheets/d/e/2PACX-1vTyLVBo6EOkYUWTy_ez3B6P_oJm4otkxIWb7lsur-_YTavztd-5ouLEKQ6at1a2OtsyHBHzlBTk1KpX/pub?gid=1742586084&single=true&output=csv', function(data) {
   const container = document.getElementById('equipe-content');
   container.innerHTML = '';
@@ -23,49 +23,7 @@ carregarDadosCSV('https://docs.google.com/spreadsheets/d/e/2PACX-1vTyLVBo6EOkYUW
         <div>
           <h3>${nome}</h3>
           <p><strong>${pessoa['Cargo']}</strong></p>
-          <p>${pessoa['Mini Currículo']}</p>
-        </div>
-      `;
-      container.appendChild(card);
-    }
-  });
-});
-
-// SEÇÃO OBRAS EM ANDAMENTO
-carregarDadosCSV('https://docs.google.com/spreadsheets/d/e/2PACX-1vTyLVBo6EOkYUWTy_ez3B6P_oJm4otkxIWb7lsur-_YTavztd-5ouLEKQ6at1a2OtsyHBHzlBTk1KpX/pub?gid=1482567846&single=true&output=csv', function(data) {
-  const container = document.getElementById('obras-andamento-content');
-  container.innerHTML = '';
-  data.forEach(obra => {
-    if (obra['Nome da Obra']?.trim()) {
-      const card = document.createElement('div');
-      card.className = 'obra';
-      card.innerHTML = `
-        <img src="${obra['URL da Imagem']}" alt="${obra['Nome da Obra']}">
-        <div>
-          <h4>${obra['Nome da Obra']}</h4>
-          <p>Área: ${obra['Área Construída']} m²</p>
-          <p>Local: ${obra['Localização']}</p>
-        </div>
-      `;
-      container.appendChild(card);
-    }
-  });
-});
-
-// SEÇÃO OBRAS CONCLUÍDAS
-carregarDadosCSV('https://docs.google.com/spreadsheets/d/e/2PACX-1vTyLVBo6EOkYUWTy_ez3B6P_oJm4otkxIWb7lsur-_YTavztd-5ouLEKQ6at1a2OtsyHBHzlBTk1KpX/pub?gid=1611364421&single=true&output=csv', function(data) {
-  const container = document.getElementById('obras-concluidas-content');
-  container.innerHTML = '';
-  data.forEach(obra => {
-    if (obra['Nome da Obra']?.trim()) {
-      const card = document.createElement('div');
-      card.className = 'obra';
-      card.innerHTML = `
-        <img src="${obra['URL da Imagem']}" alt="${obra['Nome da Obra']}">
-        <div>
-          <h4>${obra['Nome da Obra']}</h4>
-          <p>Área: ${obra['Área Construída']} m²</p>
-          <p>Local: ${obra['Localização']}</p>
+          <p>${pessoa['Mini CurrÃ­culo']}</p>
         </div>
       `;
       container.appendChild(card);
